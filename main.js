20df4f12f6355dcd9c3dcf7f42063081835283bb
s18014 <s18014@std.it-college.ac.jp>
2018-11-26 17:02:41 +0900

    弾の管理クラスを作成

--- main.js
+++ main.js
@@ -19,34 +19,28 @@ const Tank = Class.create(Sprite, {
         this.vy= 0;
         this.speed = 3;
         this.rotationSpeed = 3;
-        this.shotTime = 0;
-        this.shotInterval = 10;
+        this.bulletM = new BulletManager(5, 20);
         this.frame = 3 * 6;
         this.image = game.assets[IMAGE.tank];
-        this.on("enterframe", function() {
-            this.shotTime += 1;
-        });
         scene.addChild(this);
     },
 
     move: function(dir) {
         this.x += Math.sin(RADIAN * this.rotation * -1) * this.speed * dir;
         this.y += Math.cos(RADIAN * this.rotation * -1) * this.speed * dir;
+
+        // 画面内に戻す処理
         if (this.x > game.width - this.width) {
             this.x = game.width - this.width - 1;
-            this.vx = -this.vx;
         }
         if (this.x < 0) {
             this.x = 1;
-            this.vx = -this.vx;
         }
         if (this.y > game.height - this.height) {
             this.y = game.height - this.height - 1;
-            this.vy = -this.vy;
         }
         if (this.y < 0) {
             this.y = 1;
-            this.vy = -this.vy;
         }
     },
 
@@ -55,11 +49,7 @@ const Tank = Class.create(Sprite, {
     },
 
     shot: function() {
-        if (this.shotTime > this.shotInterval) {
-            this.bullet = new Bullet(this.x + this.width / 2, this.y + this.height / 2, this.rotation);
-            scene.insertBefore(this.bullet, this);
-            this.shotTime = 0;
-        };
+        this.bulletM.shot(this.x + this.width / 2 + Math.sin(RADIAN * this.rotation * -1) * -20, this.y + this.height / 2 + Math.cos(RADIAN * this.rotation * -1) * -20, this.rotation);
     },
 
     rotationShiftUp: function() {
@@ -75,40 +65,103 @@ const Tank = Class.create(Sprite, {
 });
 
 const Bullet = Class.create(Sprite, {
-    initialize: function (x, y, rotation) {
+    initialize: function() {
         Sprite.call(this, 16, 16);
         this.image = game.assets[IMAGE.icon];
         this.frame = 3 * 16 + 8;
-        this.rotation = rotation;
-        this.x = x - this.width / 2;
-        this.y = y - this.height / 2;
+        this.rotation = 0;
         this.speed = 10;
+        this.visible = false;
         this.on("enterframe", function() {
+            if (!this.visible) return;
             this.move();
-            if (this.x < 0 + this.widht || this.x > game.width || this.y < 0 - this.height || this.y > game.height) {
-                scene.removeChild(this);
+            if (this.x < 0 - this.width || this.x > game.width || this.y < 0 - this.height || this.y > game.height) {
+                this.visible = false;
             }
         });
-        scene.addChild(this);
+    },
+
+    set: function(x, y, rotation) {
+        this.x = x - this.width / 2;
+        this.y = y - this.height / 2;
+        this.rotation = rotation;
+        new Effect(x, y);
     },
 
     move: function() {
         this.x += Math.sin(RADIAN * this.rotation * -1) * this.speed * -1;
         this.y += Math.cos(RADIAN * this.rotation * -1) * this.speed * -1;
+    },
+
+    shot: function() {
+        this.visible = true;
+    }
+
+});
+
+const BulletManager = Class.create(Group, {
+    initialize: function(amount, interval) {
+        Group.call(this);
+        this.amount = amount;
+        this.make();
+        this.time = 0;
+        this.interval = interval;
+        this.on("enterframe", function() {
+            this.time += 1;
+        });
+        scene.addChild(this);
+    },
+
+    make: function() {
+        for (i=0; i<this.amount; i++) {
+            this.addChild(new Bullet());
+        }
+    },
+
+    shot: function(x, y, rotation) {
+        if (this.time < this.interval) return;
+        for (i=0; i<this.childNodes.length; i++) {
+            // 使っていない弾が在る場合に新しい座標の設定と発射命令をする
+            if (!this.childNodes[i].visible) {
+                this.childNodes[i].set(x, y, rotation)
+                this.childNodes[i].shot();
+                this.time = 0;
+                break;
+            }
+        }
     }
 });
+
+const Effect = Class.create(Sprite, {
+    initialize: function(x, y) {
+        Sprite.call(this, 16, 16);
+        this.image = game.assets[IMAGE.effect];
+        this.x = x - this.width / 2;
+        this.y = y - this.height / 2;
+        this.on("enterframe", function() {
+            if (this.frame >= 4 + (1 / 7 * 5)) scene.removeChild(this);
+            this.frame = this.age / 7 % 4 + 1;
+        });
+        scene.addChild(this);
+    }
+});
+
+class Vector2d {
+    constructor() {
+    }
+}
 
 const main = () => {
     game = new Core();
     game.fps = 60;
-    game.preload(IMAGE.tank);
-    game.preload(IMAGE.effect);
-    game.preload(IMAGE.icon);
+    game.preload([IMAGE.tank, IMAGE.effect, IMAGE.icon]);
     scene = game.rootScene;
     scene.backgroundColor = "#666";
 
     game.on('load', () => {
         const tank = new Tank(game.width / 2, game.height / 2);
+        const tank2 = new Tank(game.width / 3, game.height / 3);
+
 
         game.keybind(65, "a");
         game.keybind(68, "d");
@@ -127,6 +180,21 @@ const main = () => {
                 tank.rotationShiftUp();
             } else {
                 tank.rotationShiftDown();
+            }
+
+
+            var dir = {x: 0, y: 0};
+            var normalize = {x: 0, y: 0};
+            if (tank.within(tank2, tank2.width)) {
+                dir.x = tank.x - tank2.x;
+                dir.y = tank.y - tank2.y;
+                dis = Math.sqrt(dir.x ** 2 + dir.y ** 2);
+                normalize.x = dir.x / dis;
+                normalize.y = dir.y / dis;
+                tank.x += normalize.x * tank.speed;
+                tank.y += normalize.y * tank.speed;
+                tank2.x -= normalize.x * tank.speed / 4;
+                tank2.y -= normalize.y * tank.speed / 4;
             }
         });
     });
